f686385f81be674e2e86d0215a4aa444a72d1b51
flend <flend@compsoc.net>
2022-02-22 12:26:23 +0000

    Ensuring non-metered items aren't checked for thresholds

--- src/brogue/Items.c
+++ src/brogue/Items.c
@@ -651,7 +651,8 @@ void populateItems(short upstairsX, short upstairsY) {
         } else {
             // Guarantee any metered items that reach generation thresholds
             for (j = 0; j < NUMBER_METERED_ITEMS; j++) {
-                if (rogue.meteredItems[j].numberSpawned * meteredItemsGenerationTable[j].genMultiplier + meteredItemsGenerationTable[j].genIncrement <
+                if (meteredItemsGenerationTable[j].initialFrequency != 0 &&
+                    rogue.meteredItems[j].numberSpawned * meteredItemsGenerationTable[j].genMultiplier + meteredItemsGenerationTable[j].genIncrement <
                     rogue.depthLevel * meteredItemsGenerationTable[j].levelScaling + randomDepthOffset) {
                         theCategory = meteredItemsGenerationTable[j].category;
                         theKind = meteredItemsGenerationTable[j].kind;
